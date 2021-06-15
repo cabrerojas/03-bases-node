0211deba19331da1720709b680e6ad1e48b4b70f
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-06-15 00:09:38 -0400

    fs
    	Uso del File System con Node

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const fs = require("fs");
+const base = 9;
+
 // Imprimir tabla del 5
+
 console.clear();
 
-const base = 5;
+console.log("===================");
+console.log(`    Tabla del ${base}    `);
+console.log("===================");
+
+let salida = "";
+
 for (let i = 1; i <= 10; i++) {
-  console.log(`${base} x ${i} = ${base * i}`);
+  salida += `${base} x ${i} = ${base * i}\n`;
 }
+
+console.log(salida);
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+  if (err) throw err;
+
+  console.log(`tabla-${base}.txt creada.`);
+});
