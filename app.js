a48225a652e3a3f7af98cd6d2c589438100b079c
Guillermo Cabrera <gcabrerojas@gmail.com>
2021-06-18 22:58:41 -0400

    yargs

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 
 const { crearArchivo } = require('./helper/multiplicar');
-// Imprimir tabla del 5
+const argv = require('yargs').argv;
 
 console.clear();
 
-// argumentos por consola por posicion
-const [,,arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+// // argumentos por consola por posicion
+// const [,,arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
+
+console.log(process.argv);
+console.log(argv);
+
+console.log('base: yargs', argv.base);
 
 
 //const base = 9;
 
-crearArchivo(base)
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch( err => console.log(err));
+// crearArchivo(base)
+//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch( err => console.log(err));
